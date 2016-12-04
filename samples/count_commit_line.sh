b7a19793d7da19ef7f413a4152b7055babab081e
laikuaut <laikuaut@gmail.com>
2016-12-05 01:24:26 +0900

    gitでコミットしたファイルの行数をカウントするスクリプトサンプル追加

---
+++ samples/count_commit_line.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ue
+
+##
+# gitにコミットされている行数を数える
+# 参考 : http://qiita.com/Night___/items/359ff81f358968567a45
+
+if [[ $# == 1 ]];then
+  since=$(date -d `expr ${1} - 1` +"%Y-%m-%d")
+  until=$(date -d ${1} +"%Y-%m-%d")
+else
+  today=$(date +"%Y%m%d")
+  since=$(date -d `expr ${today} - 1` +"%Y-%m-%d")
+  until=$(date -d ${today} +"%Y-%m-%d")
+fi
+
+log_dir=~/log/${since}_${until}
+mkdir -p ${log_dir}
+
+all_count_file=${log_dir}/all_count.log
+:> ${all_count_file}
+git_log_cmd="git log --numstat --date=short --pretty=\"[%cd] [%h] [%an] %s\" --author='laikuaut' --since=${since} --until=${until} --no-merges"
+find -mindepth 1 -maxdepth 1 -type d | while read line
+do
+  pushd ${line} >& /dev/null
+  log_file=${log_dir}/${line#./}.log
+  :> ${log_file}
+  echo -n "# ${line#./}" | tee -a ${log_file}
+  echo -n " : "
+  echo >> ${log_file}
+#  echo ${git_log_cmd}
+  git log --numstat --date=short --pretty="[%cd] [%h] [%an] %s" --author='laikuaut' --since=${since} --until=${until} --no-merges >> ${log_file}
+  git log --numstat --date=short --pretty="[%cd] [%h] [%an] %s" --author='laikuaut' --since=${since} --until=${until} --no-merges | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("%d (+%d, -%d)\n", plus+minus, plus, minus)}' | tee -a ${all_count_file}
+  popd >& /dev/null
+done
