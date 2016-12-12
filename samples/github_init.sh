faf392ff3aaabb4e67d1c44638de954574d21d54
laikuaut <laikuaut@gmail.com>
2016-12-12 22:45:13 +0900

    gistとgithubで新規作成したリポジトリをローカルへ持ってくる初期化スクリプト

---
+++ samples/github_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ue
+
+if [[ $# != 1 ]];then
+    echo "引数不足"
+    echo "リポジトリ名を指定してください。"
+    exit 1
+fi
+
+repo_name=${1}
+user_name=laikuaut
+
+mkdir ${repo_name} -p
+cd ${repo_name}
+git init
+git remote add origin https://github.com/${user_name}/${repo_name}.git
+git remote set-url origin git@github.com:${user_name}/${repo_name}.git
+git pull origin master
